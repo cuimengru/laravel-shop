cc724b1833f654d2b67166b31174d37ef498d93d
vagrant <vagrant@homestead>
2020-06-12 10:16:06 +0000

    修改和删除收货地址

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,10 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
     //增加收货地址的方法
     Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+    //修改收货地址的页面
+    Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+    //修改收货地址的方法
+    Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+    //删除收货地址
+    Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 });
