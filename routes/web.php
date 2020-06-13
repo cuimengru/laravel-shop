519a6b97e19b0dedc1eaf3d36c0b1cf5e7e66b7a
vagrant <vagrant@homestead>
2020-06-13 06:11:23 +0000

    用户界面 - 商品收藏

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     //删除收货地址
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+    //收藏商品
+    Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+    //取消收藏
+    Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 });
