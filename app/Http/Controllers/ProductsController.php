0cc43e5f192984b55a20e14f19bfc002d9094fd3
vagrant <vagrant@homestead>
2020-06-13 03:08:45 +0000

    用户界面 - 商品筛选、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,11 +9,46 @@ class ProductsController extends Controller
 {
     //商品列表页
     public function index(Request $request){
+        //构建一个查询构造器,//where('on_sale', true) 筛选出 on_sale 字段为 true 的记录，这样未上架的商品就不会被展示出来
+        $builder = Product::query()->where('on_sale',true);
+        //判断是否有提交 search 参数，如果有就赋值给 $search 变
+        //search 参数用来模糊搜索商品
+        if ($search = $request->input('search','')){
+            $like = '%'.$search.'%';
+            //模糊搜索商品标题、商品详情、SKU标题、SKU描述
+            $builder->where(function ($query) use ($like){
+               $query->where('title','like',$like)
+                    ->orWhere('description','like',$like)
+                    ->orWhereHas('skus',function ($query) use ($like){
+                        $query->where('title','like',$like)
+                            ->orWhere('description','like',$like);
+                    });
+            });
+        }
 
-        //where('on_sale', true) 筛选出 on_sale 字段为 true 的记录，这样未上架的商品就不会被展示出来
-        $products = Product::query()->where('on_sale', true)->paginate(16);
+        //是否有提交 order 参数，如果有就赋值给 $order 变量
+        // order 参数用来控制商品的排序规则
+        if ($order = $request->input('order','')){
+            //是否是以 _asc 或者 _desc 结尾
+            if (preg_match('/^(.+)_(asc|desc)$/',$order,$m)){
+                //如果字符串的开头是这3个字符串之一，说明是一个合法的排序值
+                if (in_array($m[1],['price','sold_count','rating'])){
+                    //根据传入的排序值来构造排序参数
+                    $builder->orderBy($m[1],$m[2]);
+                }
 
-        return view('products.index', ['products' => $products]);
+            }
+        }
+
+
+        $products = $builder->paginate(16);
+
+        return view('products.index', [
+                'products' => $products,'filters'  => [
+                'search' => $search,
+                'order'  => $order,
+                ],
+            ]);
     }
 
 }
